a36159886f768a1ec0a02cea42d0e66f1dc6fbf6
Alex Zinchenko <admloki@gmail.com>
2014-11-25 13:16:15 +0300

    Added etcd dep [WIP]

---
+++ etcd.rb
@@ -0,0 +1,29 @@
+dep 'etcd', :version, :patchlevel do
+    version.default! '0.5.0'
+    patchlevel.default! 'alpha.3'
+    prefix.default!(shell('whoami') == 'root' ? '/usr/bin' : '~/bin')
+
+    def version_group
+        "#{version}-#{patchlevel}"
+    end
+
+    def osdep_group
+        "#{Babushka.host.linux? ? 'linux' : 'darwin'}-amd64"
+    end
+
+    def source_uri
+        "https://github.com/coreos/etcd/releases/download/v#{version_group}/etcd-v#{version_group}-#{osdep_group}.tar.gz"
+    end
+
+    def binaries
+        ['etcd', 'etcdctl', 'etcd-migrate']
+    end
+
+    def binaries_presents?
+        binaries.select {|binary| File.executable?(prefix / binary)}
+    end
+
+    met? {
+        in_path? 'etcd' and binaries_presents?
+    }
+end
